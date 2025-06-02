4df0d158136d9e110b56ece53854a794d2aa2836
dprashanth-kumar <dprashanthtinku@gmail.com>
2025-06-02 21:13:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,3 +41,10 @@ echo "Installation completed successfully."
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
+#check versions
+aws --version
+eksctl version
+kubectl version --client
+kubectl version
+docker --version
